6c6cb603f293be6d070a5b8891425439444e6f4e
Mario Bojarovski <bojarovskimario@gmail.com>
2024-03-13 22:17:56 +0100

    Initial commit

---
+++ src/main/java/com/damilah/coffeeShop/coffeeShop/common/Coach.java
@@ -0,0 +1,6 @@
+package com.damilah.coffeeShop.coffeeShop.common;
+
+public interface Coach {
+    String getDailyWorkout();
+
+}
---
+++ src/test/java/com/damilah/coffeeShop/coffeeShop/CoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damilah.coffeeShop.coffeeShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
